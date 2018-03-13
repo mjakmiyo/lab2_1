6a3acc122fdadd820a298af4cfb42306ea9b07b4
gronek.grzegorz <202154@edu.p.lodz.pl>
2018-03-13 18:23:22 +0100

    testy_v1 added isMiddleElement test

--- src/test/java/edu/iis/mto/bsearch/TestSearch.java
+++ src/test/java/edu/iis/mto/bsearch/TestSearch.java
@@ -33,8 +33,17 @@ public class TestSearch {
     @Test
     public void isLastElement() {
         int key = 55;
-        int position = 12;
+        int position = 10;
         int seq[] = {1, 5, 6, 9, 10, 15, 20, 25, 40, 55};
+        SearchResult search = BinarySearch.search(key, seq);
+        Assert.assertEquals(position, search.getPosition());
+    }
+
+    @Test
+    public void isMiddleElement() {
+        int key = 9;
+        int position = 4;
+        int seq[] = {1, 5, 6, 9, 10, 18, 31};
         SearchResult search = BinarySearch.search(key, seq);
         Assert.assertEquals(position, search.getPosition());
     }
