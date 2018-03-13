834784fc53c27b2ba19e6731cda01e5ecfdf5ebd
gronek.grzegorz <202154@edu.p.lodz.pl>
2018-03-13 18:16:36 +0100

    testy_v1 added isFirstElement test

--- src/test/java/edu/iis/mto/bsearch/TestSearch.java
+++ src/test/java/edu/iis/mto/bsearch/TestSearch.java
@@ -21,4 +21,13 @@ public class TestSearch {
         Assert.assertFalse(search.isFound());
     }
 
+    @Test
+    public void isFirstElement() {
+        int key = 1;
+        int position = 1;
+        int seq[] = {1, 5, 6, 9, 10, 15, 17, 20, 25, 30, 40, 55};
+        SearchResult search = BinarySearch.search(key, seq);
+        Assert.assertEquals(position, search.getPosition());
+    }
+
 }
