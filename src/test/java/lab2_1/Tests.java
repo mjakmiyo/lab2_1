1f8e94f21f67743c015b8d0de62fb671f9cdd9a6
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-15 14:55:29 +0100

    testy_v1 Length1ShouldNotBeInSequence

--- src/test/java/lab2_1/Tests.java
+++ src/test/java/lab2_1/Tests.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 import edu.iis.mto.bsearch.BinarySearch;
 
 public class Tests {
+	
 	@Test
 	public void Length1ShouldBeInSequence() {
-		assertEquals(true, BinarySearch.search(5, new int[] {5}).isFound());
+		assertTrue(BinarySearch.search(5, new int[] {5}).isFound());
+	}
+	
+	@Test
+	public void Length1ShouldNotBeInSequence() {
+		assertFalse(BinarySearch.search(5, new int[] {0}).isFound());
 	}
 }
