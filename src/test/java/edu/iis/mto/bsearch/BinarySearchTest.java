24c3d66dcb8e5c99c5f8caf654c889a6a12ea702
Mateusz Sędkowski <mateusz.sedkowski@gmail.com>
2018-03-18 20:03:49 +0100

    testy_v1. Remove code duplication.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static edu.iis.mto.bsearch.BinarySearch.search;
@@ -7,70 +8,54 @@ import static org.junit.Assert.*;
 
 public class BinarySearchTest {
 
+
+    private static int size = (int) Math.ceil(Math.random() * 100 + 2);
+    private static int[] seq;
+
+    @Before
+    public void setUp() {
+        seq = new int[size];
+        for(int i = size; i > 0; i--) {
+            seq[i - 1] = i;
+        }
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void showErrorWhenSeqIsEmpty() throws IllegalArgumentException{
-        int key = 0;
-        int[] seq = new int[0];
-        search(key, seq);
+        search(0, new int[0]);
     }
 
     @Test
     public void findElementInSeqWhenSeqLengthIsOne(){
-        int key = 1;
-        int[] seq = new int[1];
+        seq = new int[1];
         seq[0] = 1;
-        assertTrue(search(key, seq).isFound());
+        assertTrue(search(1, seq).isFound());
     }
 
     @Test
     public void notFindElementInSeqWhenSeqLengthIsOne(){
-        int key = 2;
-        int[] seq = new int[1];
+        seq = new int[1];
         seq[0] = 1;
-        assertTrue(!search(key, seq).isFound());
+        assertTrue(!search(2, seq).isFound());
     }
 
     @Test
     public void notFindElementInSeqWhenSeqLengthIsBiggerThanOne(){
-        int key = 250;
-        int size = (int) Math.ceil(Math.random() * 100 + 2);
-        int[] seq = new int[size];
-        for(; size > 0; size--) {
-            seq[size - 1] = size;
-        }
-        assertTrue(!search(key, seq).isFound());
+        assertTrue(!search(250, seq).isFound());
     }
 
     @Test
     public void findElementInSeqOnFirstPositionWhenSeqLengthIsBiggerThanOne(){
-        int size = (int) Math.ceil(Math.random() * 100 + 2);
-        int[] seq = new int[size];
-        for(; size > 0; size--) {
-            seq[size - 1] = size;
-        }
-        int key = seq[0];
-        assertTrue(search(key, seq).getPosition() == 1);
+        assertTrue(search(seq[0], seq).getPosition() == 1);
     }
 
     @Test
     public void findElementInSeqOnLastPositionWhenSeqLengthIsBiggerThanOne(){
-        int size = (int) Math.ceil(Math.random() * 100 + 2);
-        int[] seq = new int[size];
-        for(; size > 0; size--) {
-            seq[size - 1] = size;
-        }
-        int key = seq[seq.length-1];
-        assertTrue(search(key, seq).getPosition() == seq.length);
+        assertTrue(search(seq[seq.length-1], seq).getPosition() == seq.length);
     }
 
     @Test
     public void findElementInSeqOnMiddlePositionWhenSeqLengthIsBiggerThanOne(){
-        int size = (int) Math.ceil(Math.random() * 100 + 2);
-        int[] seq = new int[size];
-        for(; size > 0; size--) {
-            seq[size - 1] = size;
-        }
-        int key = seq[(seq.length-1)/2];
-        assertTrue(search(key, seq).getPosition() == (seq.length+1)/2);
+        assertTrue(search(seq[(seq.length-1)/2], seq).getPosition() == (seq.length+1)/2);
     }
 }
