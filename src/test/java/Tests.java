1f99ef823b3a553eacc6ca63d6e660963902ef76
Bartosz Suśniło <susnilox@gmail.com>
2018-03-19 16:01:49 +0100

    testy_v2 Changes in tests (assertThat)

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,6 +2,9 @@ import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 /*
 
 Długość sekwencji wejściowej
@@ -28,59 +31,71 @@ public class Tests {
     @Test
     public void isInOneElementSequence(){
         seq=new int[1];
-        seq[0]=3;
-        Assert.assertEquals(true,BinarySearch.search(3, seq).isFound());
+        int value = 3;
+        seq[0]=value;
+        result = BinarySearch.search(value, seq);
+        Assert.assertThat(result.isFound(),is(true));
     }
     @Test
     public void isNotInOneElementSequence(){
         seq=new int[1];
-        seq[0]=4;
-        Assert.assertEquals(false,BinarySearch.search(3, seq).isFound());
+        int value = 4;
+        seq[0]=value;
+        result = BinarySearch.search(value+1, seq);
+        Assert.assertThat(result.isFound(),is(false));
+        Assert.assertThat(result.getPosition(),is(-1));
     }
     @Test
     public void isFirstInManyElementSequence(){
         seq=new int[2];
-        seq[0]=4;
+        int value = 4;
+        final int EXPECTED_POSITION = 0;
+        seq[0]=value;
         seq[1]=23;
-        result = BinarySearch.search(4, seq);
-        Assert.assertEquals(1,result.getPosition());
+        result = BinarySearch.search(value, seq);
+        Assert.assertThat(result.isFound(), is(true));
+        Assert.assertThat(result.getPosition(), is(EXPECTED_POSITION));
     }
     @Test
     public void isLastInManyElementSequence(){
         seq=new int[4];
+        int value = 23;
+        final int EXPECTED_POSITION = seq.length - 1;
         seq[0]=4;
         seq[1]=5;
         seq[2]=6;
-        seq[3]=23;
-        result = BinarySearch.search(23, seq);
-        Assert.assertEquals(seq.length,result.getPosition());
+        seq[3]=value;
+        result = BinarySearch.search(value, seq);
+        Assert.assertThat(result.isFound(), is(true));
+        Assert.assertThat(result.getPosition(), is(EXPECTED_POSITION));
     }
     @Test
     public void isMiddleInManyOddElementSequence(){
         seq=new int[3];
+        int value = 23;
+        final int EXPECTED_POSITION = seq.length/2;
         seq[0]=4;
         seq[1]=15;
-        seq[2]=23;
+        seq[2]=value;
         result = BinarySearch.search(15, seq);
-        //Check if length of sequence is odd
-        Assert.assertEquals(15, result.getCenter());
-        if(seq.length % 2 == 1){
-            Assert.assertEquals(seq.length-seq.length/2,result.getPosition());
-        }
-        Assert.assertTrue((seq.length-seq.length/2 == result.getPosition()| seq.length/2== result.getPosition()));
-        //seq.length/2
+        result = BinarySearch.search(value, seq);
+        Assert.assertThat(result.isFound(), is(true));
+        Assert.assertThat(result.getCenter(), is(EXPECTED_POSITION));
     }
     @Test
     public void isNotInManyElementSequence(){
         seq=new int[6];
+        int value = 7;
+        final int EXPECTED_POSITION = -1;
         seq[0]=4;
         seq[1]=15;
         seq[2]=23;
         seq[3]=24;
         seq[4]=115;
         seq[5]=223;
-        result = BinarySearch.search(7, seq);
-        Assert.assertEquals(false,result.isFound());
+        result = BinarySearch.search(value, seq);
+        Assert.assertThat(result.isFound(), is(false));
+        Assert.assertThat(result.getPosition(), is(EXPECTED_POSITION));
     }
 }
 /*
