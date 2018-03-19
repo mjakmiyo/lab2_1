dfd497a953815c71b23bd46dc153f0256421be04
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-19 17:26:07 +0100

    testy_v2 @Before i @After

--- src/test/java/lab2_1/Tests.java
+++ src/test/java/lab2_1/Tests.java
@@ -2,39 +2,59 @@ package lab2_1;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
 
 public class Tests {
 	
+	int[] oneElementArray;
+	int[] biggerArrayOdd;
+	int[] biggerArrayEven;
+	
+	@Before
+	public void testData() {
+		 this.oneElementArray = new int[] {5};
+		 this.biggerArrayOdd = new int[] {1,2,3,4,5};
+		 this.biggerArrayEven = new int[] {1,2,3,4,5,6};
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		this.oneElementArray = null;
+		this.biggerArrayOdd = null;
+		this.biggerArrayEven = null;
+	}
+	
 	@Test
 	public void Length1ShouldBeInSequence() {
-		assertTrue(BinarySearch.search(5, new int[] {5}).isFound());
+		assertTrue(BinarySearch.search(5, oneElementArray).isFound());
 	}
 	
 	@Test
 	public void Length1ShouldNotBeInSequence() {
-		assertFalse(BinarySearch.search(5, new int[] {0}).isFound());
+		assertFalse(BinarySearch.search(1, oneElementArray).isFound());
 	}
 	
 	@Test
 	public void ShouldBeFirst() {
-		assertEquals(1, BinarySearch.search(1, new int[] {1,3,2,1}).getPosition());
+		assertEquals(1, BinarySearch.search(1, biggerArrayOdd).getPosition());
 	}
 	
 	@Test
-	public void ShouldBeLast() {
-		assertEquals(5 , BinarySearch.search(5, new int[] {4,1,2,3,5}).getPosition());
+	public void ShouldBeLast() {		
+		assertEquals(5 , BinarySearch.search(5, biggerArrayOdd).getPosition());
 	}
 	
 	@Test
 	public void ShouldBeInTheMiddle() {
-		assertEquals(2 , BinarySearch.search(2, new int[] {1,2,4}).getPosition());
+		assertEquals(3 , BinarySearch.search(3, biggerArrayOdd).getPosition());
 	}
 	
 	@Test
 	public void NotInSequence() {
-		assertEquals(false , BinarySearch.search(8, new int[] {1,2,4,11,12,15}).isFound());
+		assertEquals(false , BinarySearch.search(8, biggerArrayOdd).isFound());
 	}
 }
