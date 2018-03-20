8d25adc0c16b5295b0712e2a945108089b3a3e9a
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-03-20 17:38:50 +0100

    testy_v2

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.bsearch;
 
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class BinarySearchTest {
@@ -7,33 +10,43 @@ public class BinarySearchTest {
 	int testarray[] = {1, 3, 4, 5, 6, 7, 8};
 	
 	@Test
-	public void ElementFoundinSequence(){
-		
+	public void ElementFoundinSequence()
+	{
+		assertThat(BinarySearch.search(1, testarray).isFound(), Matchers.is(true));
 	}
 	
 	@Test
-	public void ElementNotFoundinSequence(){
-		
+	public void ElementNotFoundinSequence()
+	{
+		assertThat(BinarySearch.search(9, testarray).isFound(), Matchers.is(false));
 	}
 	
 	@Test
-	public void ElementIsAFirstElement(){
-		
+	public void IsAFirstElement()
+	{
+		int element = 1;
+		assertThat(BinarySearch.search(element, testarray).getPosition() -1, Matchers.is(element));
 	}
 	
 	@Test
-	public void ElementIsALastElement(){
-		
+	public void IsALastElement()
+	{
+		int element = 8 ;
+		assertThat(BinarySearch.search(element, testarray).getPosition() -1, Matchers.is(element));
 	}
 	
 	@Test
-	public void ElementIsAMiddleElement(){
-		
+	public void IsAMiddleElement()
+	{
+		int element = testarray.length/2 +1;
+		assertThat(BinarySearch.search(element, testarray).getPosition() -1, Matchers.is(element));
 	}
 	
 	@Test
-	public void ElementNotFoundinSquenceButLargerThanOne(){
-		
+	public void ElementNotFoundinSquenceButLargerThanOne()
+	{
+		int element = 9;
+		assertThat(BinarySearch.search(element, testarray).getPosition() -1, Matchers.is(element));
 	}
 	
 }
