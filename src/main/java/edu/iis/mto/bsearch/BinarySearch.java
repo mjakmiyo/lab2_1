74221c79fc8fa0193d9de5fc8204ea3f92ecf11a
Rafikus <rafikus95@gmail.com>
2018-03-20 17:10:35 +0100

    testy_v1 Added class BinarySearchTest.java

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -26,6 +26,11 @@ public class BinarySearch {
 		int center;
 		SearchResult result = new SearchResult();
 
+		if (seq.length == 0) 
+		{
+			throw new IllegalArgumentException();
+		}
+		
 		while (start <= end) {
 			center = (start + end) / 2;
 			if (seq[center] == key) {
---
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,39 @@
+package edu.iis.mto.bsearch;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+	
+	int testarray[] = {1, 3, 4, 5, 6, 7, 8};
+	
+	@Test
+	public void ElementFoundinSequence(){
+		
+	}
+	
+	@Test
+	public void ElementNotFoundinSequence(){
+		
+	}
+	
+	@Test
+	public void ElementIsAFirstElement(){
+		
+	}
+	
+	@Test
+	public void ElementIsALastElement(){
+		
+	}
+	
+	@Test
+	public void ElementIsAMiddleElement(){
+		
+	}
+	
+	@Test
+	public void ElementNotFoundinSquenceButLargerThanOne(){
+		
+	}
+	
+}
