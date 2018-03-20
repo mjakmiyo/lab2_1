2ee69c28654e301e559ed381f30c345fcdd0461d
Jakub Mankowski <jakub.mankowski20@gmail.com>
2018-03-20 13:37:34 +0100

    testy_v2 change assertTrue to assertThat

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import static edu.iis.mto.bsearch.BinarySearch.search;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class BinarySearchTest {
@@ -18,7 +20,7 @@ public class BinarySearchTest {
     public void keyIsInOneElementSequence() {
         int[] seq = {3};
         int key = 3;
-        assertTrue( search( key, seq ).isFound() && search( key, seq ).getPosition() == 1 );
+        assertThat( search( key, seq ).isFound() && search( key, seq ).getPosition() == 1, is(true) );
     }
 
 
@@ -26,34 +28,34 @@ public class BinarySearchTest {
     public void keyIsNotInOneElementSequence() {
         int[] seq = {1};
         int key = 2;
-        assertTrue( !search( key, seq ).isFound() && search( key, seq ).getPosition() == -1 );
+        assertThat( !search( key, seq ).isFound() && search( key, seq ).getPosition() == -1, is(true) );
     }
 
     @Test
     public void keyIsFirstElementInSequence() {
         int[] seq = {1, 2, 3, 5};
         int key = 1;
-        assertTrue( search( key, seq ).isFound() && search( key, seq ).getPosition() == 1 );
+        assertThat( search( key, seq ).isFound() && search( key, seq ).getPosition() == 1 , is(true));
     }
 
     @Test
     public void keyIsLastElementInSequence() {
         int[] seq = {1, 2, 4, 5, 6, 10, 13, 15};
         int key = 15;
-        assertTrue( search( key, seq ).isFound() && search( key, seq ).getPosition() == seq.length );
+        assertThat( search( key, seq ).isFound() && search( key, seq ).getPosition() == seq.length, is(true));
     }
 
     @Test
     public void keyIsMiddleElementInSequence() {
-        int[] seq = {1, 2, 4, 5, 10, 11, 13, 14, 15, 20, 22};
-        int key = 11;
-        assertTrue( search( key, seq ).isFound() && search( key, seq ).getPosition() == 6 );
+        int[] seq = {1, 2, 4, 5, 10, 11, 13, 14, 15, 20};
+        int key = 10;
+        assertThat( search( key, seq ).getPosition(), is((seq.length+1)/2));
     }
 
     @Test
     public void keyIsNotInSequence() {
         int[] seq = {1, 2, 4, 5, 6, 10, 13, 15, 16, 19};
         int key = 20;
-        assertTrue( !search( key, seq ).isFound() && search( key, seq ).getPosition() == -1 );
+        assertThat( !search( key, seq ).isFound() && search( key, seq ).getPosition() == -1 , is(true));
     }
 }
