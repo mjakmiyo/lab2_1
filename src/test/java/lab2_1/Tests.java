8dc8a7f0a2e02e2d55acc20041f60271a1c7a8d2
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-24 09:54:56 +0100

    testy_v2 klaponowna refraktoryzacja

--- src/test/java/lab2_1/Tests.java
+++ src/test/java/lab2_1/Tests.java
@@ -8,63 +8,87 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 
-public class Tests {
+public class Tests {	
+	
+	@Test(expected = IllegalArgumentException.class )
+    public void SequenceShouldBeEmpty(){
+		SearchResult result = new SearchResult();
+        int[] sequence = new int[0];
+        int key = 3;
+        result = BinarySearch.search(key, sequence);
+
+    }
 	
 	@Test
 	public void ShouldBeInOneElementSequence() {
 		final boolean EXPECTED = true;
+		SearchResult result = new SearchResult();
 		int key = 3;
-		int[]  oneElementArray = new int[] {3};
-		assertThat(EXPECTED,is(BinarySearch.search(key, oneElementArray).isFound()));
+		int[]  singleElementSequence = new int[] {3};
+		result = BinarySearch.search(key, singleElementSequence);
+		assertThat(EXPECTED,is(result.isFound()));
 	}
 	
 	@Test
 	public void ShouldNotBeInOneElementSequence() {
 		final boolean EXPECTED = false;
+		SearchResult result = new SearchResult();
 		int key = 5;
-		int[] oneElementArray = new int[] {3};
-		assertThat(EXPECTED,is(BinarySearch.search(key, oneElementArray).isFound()));
+		int[] singleElementSequence = new int[] {3};
+		result = BinarySearch.search(key, singleElementSequence);
+		assertThat(EXPECTED,is(result.isFound()));
 	}
 	
 	@Test
 	public void ShouldBeFirst() {
 		final int EXPECTED = 1;
+		SearchResult result = new SearchResult();
 		int key = 1;
-		int[] biggerArray = new int[] {1,2,3,4,5};
-		assertThat(EXPECTED, is(BinarySearch.search(key, biggerArray).getPosition()));
+		int[] multiElementsSequence = new int[] {1,2,3,4,5};
+		result = BinarySearch.search(key, multiElementsSequence);
+		assertThat(EXPECTED, is(result.getPosition()));
 	}
 	
 	@Test
 	public void ShouldBeLast() {	
 		int key = 9;
-		int[] biggerArray = new int[] {5,6,7,8,9};
-		final int EXPECTED = biggerArray.length;
-		assertThat(EXPECTED, is(BinarySearch.search(key, biggerArray).getPosition()));
+		SearchResult result = new SearchResult();
+		int[] multiElementsSequence = new int[] {5,6,7,8,9};
+		final int EXPECTED = multiElementsSequence.length;
+		result = BinarySearch.search(key, multiElementsSequence);
+		assertThat(EXPECTED, is(result.getPosition()));
 	}
 	
 	@Test
 	public void ShouldBeInTheMiddleOddArray() {
 		int key = 17;
-		int[] biggerArray = new int[] {15,16,17,18,19};
-		final int EXPECTED = (biggerArray.length/2)+1;
-		assertThat(EXPECTED, is(BinarySearch.search(key, biggerArray).getPosition()));
+		SearchResult result = new SearchResult();
+		int[] multiElementsSequence = new int[] {15,16,17,18,19};
+		final int EXPECTED = (multiElementsSequence.length/2)+1;
+		result = BinarySearch.search(key, multiElementsSequence);
+		assertThat(EXPECTED, is(result.getPosition()));
 	}
 	
 	@Test
 	public void ShouldBeInTheMiddleEvenArray() {
 		int key = 118;
-		int[] biggerArray = new int[] {115,116,117,118,119,120};
-		final int EXPECTED = (biggerArray.length/2)+1;
-		assertThat(EXPECTED, is(BinarySearch.search(key, biggerArray).getPosition()));
+		SearchResult result = new SearchResult();
+		int[] multiElementsSequence = new int[] {115,116,117,118,119,120};
+		final int EXPECTED = (multiElementsSequence.length/2)+1;
+		result = BinarySearch.search(key, multiElementsSequence);
+		assertThat(EXPECTED, is(result.getPosition()));
 	}
 	
 	@Test
 	public void ShouldNotBeInSequence() {
 		int key = 17;
-		int[] biggerArray = new int[] {1,2,3,4,5};
+		SearchResult result = new SearchResult();
+		int[] multiElementsSequence = new int[] {1,2,3,4,5};
 		final boolean EXPECTED = false;
-		assertThat(EXPECTED, is(BinarySearch.search(key, biggerArray).isFound()));
+		result = BinarySearch.search(key, multiElementsSequence);
+		assertThat(EXPECTED, is(result.isFound()));
 	}
 	
 	@Test
@@ -72,8 +96,14 @@ public class Tests {
 		int key = 1;
 		int key2 = 3;
 		int key3 = 5;
-		int[] biggerArray = new int[] {1,2,3,4,5};
+		SearchResult result = new SearchResult();
+		SearchResult result2 = new SearchResult();
+		SearchResult result3 = new SearchResult();
+		int[] multiElementsSequence = new int[] {1,2,3,4,5};
 		final boolean EXPECTED = true;
-		assertThat(EXPECTED, allOf(is(BinarySearch.search(key, biggerArray).isFound()), is(BinarySearch.search(key2, biggerArray).isFound()), is(BinarySearch.search(key3, biggerArray).isFound())));
+		result = BinarySearch.search(key, multiElementsSequence);
+		result2 = BinarySearch.search(key2, multiElementsSequence);
+		result3 = BinarySearch.search(key3, multiElementsSequence);
+		assertThat(EXPECTED, allOf(is(result.isFound()), is(result2.isFound()), is(result3.isFound())));
 	}
 }
