0f93c613ab24cb8bc9fe105366d9485395e10da4
Marcin Kapelan <marcinkape@gmail.com>
2018-03-24 20:15:36 +0100

    testy_v1 [Change input data according to preconditions] -array has to be sorted in ascending order

--- src/test/java/BinarySearchTests.java
+++ src/test/java/BinarySearchTests.java
@@ -6,26 +6,26 @@ import org.junit.Test;
 public class BinarySearchTests {
     @Test
     public void shouldFoundKeyInSequenceWithLength1() {
-        Assert.assertTrue(BinarySearch.search(1, new int[] {1}).isFound());
+        Assert.assertTrue(BinarySearch.search(1, new int[] {1, 2 ,3}).isFound());
     }
     @Test
     public void shouldNotFoundKeyInSequenceWithLength1() {
-        Assert.assertFalse(BinarySearch.search(1, new int[] {5}).isFound());
+        Assert.assertFalse(BinarySearch.search(1, new int[] {5, 8, 12, 40}).isFound());
     }
     @Test
     public void soughtKeyShouldBeFirstOnElementList() {
-        Assert.assertEquals(1 ,BinarySearch.search(1, new int[] {1, 5, 3}).getPosition());
+        Assert.assertEquals(1 ,BinarySearch.search(1, new int[] {1, 3, 5}).getPosition());
     }
     @Test
     public void soughtKeyShouldBeLastOnElementList() {
-        Assert.assertEquals(3 ,BinarySearch.search(3, new int[] {7, 1, 3}).getPosition());
+        Assert.assertEquals(3 ,BinarySearch.search(3, new int[] {1, 2, 3}).getPosition());
     }
     @Test
     public void soughtKeyShouldBeInMiddleOnElementList() {
-        Assert.assertEquals(3 ,BinarySearch.search(2, new int[] {7, 1, 2, 5, 9}).getPosition());
+        Assert.assertEquals(3 ,BinarySearch.search(2, new int[] {0, 1, 2, 5, 9}).getPosition());
     }
     @Test
     public void shouldFoundKeyInSequenceWithLengthBiggerThan1() {
-        Assert.assertTrue(BinarySearch.search(0, new int[] {7, 0, 2, 5}).isFound());
+        Assert.assertTrue(BinarySearch.search(12, new int[] {1, 2, 4, 5, 12, 14}).isFound());
     }
 }
