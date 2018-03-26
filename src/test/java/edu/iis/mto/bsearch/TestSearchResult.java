740427c1c8801d229155578a6c803a52f8bbc546
rogo6 <piotrrogozinski95@gmail.com>
2018-03-26 18:08:48 +0200

    testy_v2. Added longer sequences

--- src/test/java/edu/iis/mto/bsearch/TestSearchResult.java
+++ src/test/java/edu/iis/mto/bsearch/TestSearchResult.java
@@ -26,7 +26,7 @@ public class TestSearchResult {
 	@Test
 	public void isFirstInSequence() {
 		int key=7;
-		int seq[]= {7,8,9};
+		int seq[]= {7,8,9,13,20,55,111};
 		int index=1;
 		searchResult=BinarySearch.search(key, seq);
 		Assert.assertEquals(index,searchResult.getPosition());
@@ -35,7 +35,7 @@ public class TestSearchResult {
 	@Test
 	public void isLastInSequence() {
 		int key=7;
-		int seq[]= {5,6,7};
+		int seq[]= {5,6,7,19,23,25,32,41};
 		int index=3;
 		searchResult=BinarySearch.search(key, seq);
 		Assert.assertEquals(index,searchResult.getPosition());
@@ -43,9 +43,9 @@ public class TestSearchResult {
 	
 	@Test
 	public void isMiddleInSequence() {
-		int key=6;
-		int seq[]= {5,6,7};
-		int index=2;
+		int key=20;
+		int seq[]= {5,6,7,19,20,25,43,111,120};
+		int index=5;
 		searchResult=BinarySearch.search(key, seq);
 		Assert.assertEquals(index,searchResult.getPosition());
 	}
@@ -53,7 +53,7 @@ public class TestSearchResult {
 	@Test
 	public void notFoundInSequenceWithNumberOfElementsGreaterThanOne() {
 		int key=68;
-		int seq[]= {5,6,7};
+		int seq[]= {5,6,7,34,53,99,111,122};
 		searchResult=BinarySearch.search(key, seq);
 		Assert.assertFalse(searchResult.isFound());
 	}
