7b40871a5e2fb0ed769109269ea4559814b133ff
Jakub Mankowski <jakub.mankowski20@gmail.com>
2018-03-26 22:02:30 +0200

    testy_v2 change name position to expectedPosition

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,12 +3,9 @@ package edu.iis.mto.bsearch;
 import org.junit.Before;
 import org.junit.Test;
 
-import static edu.iis.mto.bsearch.BinarySearch.isSorted;
 import static edu.iis.mto.bsearch.BinarySearch.search;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class BinarySearchTest {
 
@@ -21,74 +18,71 @@ public class BinarySearchTest {
     public void keyIsInOneElementSequence() {
         int[] seq = {3};
         int key = 3;
-        int position = 1;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = 1;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
     public void keyIsNotInOneElementSequence() {
         int[] seq = {1};
         int key = 2;
-        int position = -1;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = -1;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
     public void keyIsFirstElementInSequence() {
         int[] seq = {1, 2, 3, 5};
         int key = 1;
-        int position = 1;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = 1;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
     public void keyIsLastElementInSequence() {
         int[] seq = {1, 2, 4, 5, 6, 10, 13, 15};
         int key = 15;
-        int position = seq.length;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = seq.length;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
-    public void keyIsMiddleElementInEvenSequence() {
+    public void keyIsLeftMiddleElementInEvenSequence() {
         int[] seq = {1, 2, 4, 5, 10, 11, 13, 14, 15, 20};
         int key = 10;
-        int position = (seq.length + 1) / 2;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = 5;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
+    }
+
+    @Test
+    public void keyIsRightMiddleElementInEvenSequence() {
+        int[] seq = {1, 2, 4, 5, 10, 11, 13, 14, 15, 20};
+        int key = 11;
+        int expectedPosition = 6;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
     public void keyIsMiddleElementInOddSequence() {
         int[] seq = {1, 2, 4, 5, 6, 10, 11, 13, 14, 15, 20};
         int key = 10;
-        int position = (seq.length + 1) / 2;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = 6;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
     public void keyIsNotInSequence() {
         int[] seq = {1, 2, 4, 5, 6, 10, 13, 15, 16, 19};
         int key = 20;
-        int position = -1;
-        assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = -1;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 
     @Test
     public void keyIsNotInEmptySequence() {
         int[] seq = {};
         int key = 20;
-        int position = -1;
-        assertThat(search(key, seq).getPosition(), is(position));
-    }
-    @Test
-    public void keyIsInNotSortedSequence() {
-        int[] seq = {10, 12, 11, 5, 6, 10, 13, 4, 16, 19};
-        int key = 10;
-        int position = -1;
-        assertThat(isSorted(seq), is(false));
-        // TO DO check if sequence is sorted
-        //TO DO check if sequence have duplicates -> throw IllegalArgumentException
-        // TO DO if seq is empty get IllegalArgumentException
-        //assertThat(search(key, seq).getPosition(), is(position));
+        int expectedPosition = -1;
+        assertThat(search(key, seq).getPosition(), is(expectedPosition));
     }
 }
